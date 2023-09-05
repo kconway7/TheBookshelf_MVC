37068f12a5c3491e839829bf1bb9fdc10514d880
kconway7 <keddyconway59@gmail.com>
2023-09-05 17:04:01 -0400

    Added OrderVM and began implementing order management

---
+++ TheBookshelfWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBookshelf.DataAccess.Repository.IRepository;
+using TheBookshelf.Models;
+
+namespace TheBookshelfWeb.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+        #endregion
+    }
+}
