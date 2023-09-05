49d9bdb5fc0be35f6013c62533f1f9dce39524b5
kconway7 <keddyconway59@gmail.com>
2023-09-05 18:17:05 -0400

    Added ability to filter orders by payment type in order management

--- TheBookshelfWeb/Areas/Admin/Controllers/OrderController.cs
+++ TheBookshelfWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TheBookshelf.DataAccess.Repository.IRepository;
 using TheBookshelf.Models;
+using TheBookshelf.Utility;
 
 namespace TheBookshelfWeb.Areas.Admin.Controllers
 {
@@ -22,9 +23,31 @@ namespace TheBookshelfWeb.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+
+            }
+
+
             return Json(new { data = objOrderHeaders });
         }
         #endregion
