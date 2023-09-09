3178c04671d79abd5708dad0ade230046944f962
kconway7 <keddyconway59@gmail.com>
2023-09-09 16:35:23 -0400

    Added viewComponent to track shopping cart

--- TheBookshelfWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TheBookshelfWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -22,7 +22,6 @@ namespace TheBookshelfWeb.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            HttpContext.Session.Clear();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
