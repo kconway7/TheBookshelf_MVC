74435d91deb21ed8f3e70bcb5ed1016de1fc3559
kconway7 <keddyconway59@gmail.com>
2023-09-10 17:43:25 -0400

    Finished implementing user management

--- TheBookshelfWeb/wwwroot/js/User.js
+++ TheBookshelfWeb/wwwroot/js/User.js
@@ -14,15 +14,52 @@ function loadDataTable(){
             { data: 'company.name', "width": "15%" },
             { data: 'role', "width": "15%" },
             {
-                data: 'id', "render": function (data) {
-                    return `<div class="w-100 btn-group" role="group">
-                    <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2 rounded-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                    <a onClick=Delete('/admin/companydelete/${data}') class="btn btn-danger mx-2 rounded-2"> <i class="bi bi-trash"></i> Delete</a>
-                    </div>
+                data: {id: "id", lockoutEnd: "lockoutEnd"}, "render": function (data) {
+
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+
+                         return `<div class="text-center">
+                            <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-sm rounded-2 btn-warning text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i>" Permissions
+                            </a>
+                            <a onclick=LockUnlock('${data.id}') class="btn btn-sm rounded-2 btn-success text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-unlock-fill"></i>" Unlock
+                            </a>
+                         </div>
                     `
+                    } else{
+
+                        return `<div class="text-center">
+                            <a href="/admin/user/RoleManagement?userId=${data.id}" class="btn btn-sm rounded-2 btn-warning text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i>" Permissions
+                            </a>
+                            <a onclick=LockUnlock('${data.id}') class="btn btn-sm rounded-2 btn-danger text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-unlock-fill"></i>" Lock
+                            </a>
+                         </div>`
+                    }                
                 },
                 "width": "25%"
             }
         ]
     });
+}
+
+function LockUnlock(id) {
+
+    $.ajax({
+        type: "POST",
+        url: "/Admin/User/LockUnlock",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+        }
+    })
 }
