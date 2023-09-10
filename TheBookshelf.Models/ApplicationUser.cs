ef7de2013b80193a80dc2ab5892bd9b11e4ecc9a
kconway7 <keddyconway59@gmail.com>
2023-09-09 22:39:09 -0400

    Began implementing user management including bans

--- TheBookshelf.Models/ApplicationUser.cs
+++ TheBookshelf.Models/ApplicationUser.cs
@@ -6,17 +6,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace TheBookshelf.Models;
 public class ApplicationUser : IdentityUser
 {
-    [Required]
-    public string Name { get; set; }
+	[Required]
+	public string Name { get; set; }
 
-    public string? StreetAddress { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? PostalCode { get; set; }
+	public string? StreetAddress { get; set; }
+	public string? City { get; set; }
+	public string? State { get; set; }
+	public string? PostalCode { get; set; }
 
-    public int? CompanyId { get; set; }
-    [ForeignKey("CompanyId")]
-    [ValidateNever]
-    public Company Company { get; set; }
+	public int? CompanyId { get; set; }
+	[ForeignKey("CompanyId")]
+	[ValidateNever]
+	public Company? Company { get; set; }
+	[NotMapped]
+	public string Role { get; set; }
 }
 
---
+++ TheBookshelfWeb/wwwroot/js/User.js
@@ -0,0 +1,28 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable(){
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        columns: [
+            { data: 'name', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'company.name', "width": "15%" },
+            { data: 'role', "width": "15%" },
+            {
+                data: 'id', "render": function (data) {
+                    return `<div class="w-100 btn-group" role="group">
+                    <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2 rounded-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                    <a onClick=Delete('/admin/companydelete/${data}') class="btn btn-danger mx-2 rounded-2"> <i class="bi bi-trash"></i> Delete</a>
+                    </div>
+                    `
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
